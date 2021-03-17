45474fbba8bd6494d6ad793d7821b5018c9bf452
putrifelda <79987392+putrifelda@users.noreply.github.com>
2021-03-17 11:39:58 +0700

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    class Class
+    {
+        // propeties
+        public string DriverName { get; set; }
+        public string onDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        // method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName: {0}", DriverName);
+            Console.WriteLine("onDuty: {0}", onDuty);
+            Console.WriteLine("NumPassenger: {0}", NumPassenger);
+        }
+        
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang proses pick up passenger\n", DriverName);
+        }
+    }
+}
