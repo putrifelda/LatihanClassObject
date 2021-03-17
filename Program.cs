7e5dd9b3e69c78eb9bc1866b4689a7ee34c09636
putrifelda <79987392+putrifelda@users.noreply.github.com>
2021-03-17 11:40:36 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugastaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object Taxi
+            Taxi taxi = new Taxi();
+
+            // mengeset nilai method
+            taxi.DriverName = "Jono";
+            taxi.onDuty = true;
+            taxi.NumPassengger = 10;
+
+            // penampilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
